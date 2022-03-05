547da5f104dc996b0bd3b8e7c42294c95e4ff94c
Mariusz Topolewski <mariusz.topolewski@leaware.com>
2022-03-05 14:06:27 +0100

    Update bottom_tab_bar.dart

--- lib/widgets/bottom_tab_bar.dart
+++ lib/widgets/bottom_tab_bar.dart
@@ -18,6 +18,18 @@ class BottomTabBar extends StatelessWidget {
             child: Container(
               color: Colors.white,
               height: 120,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TabButton(),
+                  TabButton(),
+                  Container(
+                    width: 74,
+                  ),
+                  TabButton(),
+                  TabButton(),
+                ],
+              ),
             ),
           ),
         ),
@@ -109,18 +121,18 @@ class BottomMenuClipper extends CustomClipper<Path> {
     const middleButtonSize = 74.0;
     const topMargin = 20.0;
     const a = middleButtonSize;
-    const b = middleButtonSize - topMargin + 10;
+    const b = middleButtonSize - topMargin + 8;
 
     var path = Path();
 
     path.lineTo(size.width / 2 - a, 0);
     path.quadraticBezierTo(
-        size.width / 2 - a + 20, 0, size.width / 2 - a + 30, b / 2);
+        size.width / 2 - a + 20, 0, size.width / 2 - a + 32, b / 2);
 
     path.quadraticBezierTo(size.width / 2 - a + 46, b, size.width / 2, b);
 
     path.quadraticBezierTo(
-        size.width / 2 + a - 46, b, size.width / 2 + a - 30, b / 2);
+        size.width / 2 + a - 46, b, size.width / 2 + a - 32, b / 2);
 
     path.quadraticBezierTo(size.width / 2 + a - 20, 0, size.width / 2 + a, 0);
 
@@ -145,4 +157,45 @@ class BottomMenuClipper extends CustomClipper<Path> {
   }
 
   double degToRad(num deg) => deg * (Math.pi / 180.0);
+}
+
+class TabButton extends StatelessWidget {
+  const TabButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: () {},
+        child: SizedBox(
+          height: 60,
+          child: Ink(
+            child: SizedBox(
+              height: 60,
+              child: Column(
+                children: [
+                  const Icon(
+                    Icons.ac_unit,
+                    size: 36,
+                    color: AppColors.buttonRed,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    width: 8,
+                    height: 8,
+                    decoration: const BoxDecoration(
+                        color: AppColors.buttonRed,
+                        borderRadius: BorderRadius.all(Radius.circular(4))),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
